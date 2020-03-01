daec91e233a554fa207e4a6aad616200935bdcbf
AlexandreSoulard <58270807+AlexandreSoulard@users.noreply.github.com>
2020-03-01 19:46:31 +0100

    Update webinfo.php

--- THDB/webinfo.php
+++ THDB/webinfo.php
@@ -11,13 +11,13 @@ require_once("tp3-helpers.php");
 			//if(isset($argv[2])) $array = array('language' => $argv[2]);
 			else $array = array('language' => 'en');
 			if(isset($_GET['movie'])) $content = json_decode(tmdbget("movie/".$_GET['movie'], $array), true);
-			else $content = json_decode(tmdbget("movie/5549", $array), true);
+			else $content = json_decode(tmdbget("movie/550", $array), true);
 			foreach($content as $key => $value){
 				if($key == "title") {
 					echo "<strong>Title</strong> : ".$value."</br>";
 					$value = strtolower($value);
 					if(isset($_GET['movie'])) $email = "https://www.themoviedb.org/movie/".$_GET['movie']."-"."$value";
-					else $email = "https://www.themoviedb.org/movie/"."5549"."-"."$value";
+					else $email = "https://www.themoviedb.org/movie/"."550"."-"."$value";
 					$email = str_replace(" ", "-", $email);
 				}
 				if($key == "original_title") echo "<strong>Original_Title</strong> : ".$value."</br>";
