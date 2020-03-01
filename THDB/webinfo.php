067a29e214234053e3ba4fd734c130820d2ece66
AlexandreSoulard <58270807+AlexandreSoulard@users.noreply.github.com>
2020-03-01 19:55:38 +0100

    Update webinfo.php

--- THDB/webinfo.php
+++ THDB/webinfo.php
@@ -9,11 +9,12 @@ require_once("tp3-helpers.php");
 				$array["language"] = $_GET['language'];
 			}
 			else $array = array('language' => 'en');
-			if(isset($_GET['movie'])) $content = json_decode(tmdbget("movie/".$_GET['movie'], $array), true);
-			else $content = json_decode(tmdbget("movie/550", $array), true);
+			if(isset($_GET['movie'])) $content = json_decode(tmdbget("movie/".$_GET['movie'], $array), true); //On accède à tous les paramètres du film entré en argument.
+			else $content = json_decode(tmdbget("movie/550", $array), true); //Par défaut on prend le film numéroté 550 qui est Fight Club.
 			foreach($content as $key => $value){
 				if($key == "title") {
 					echo "<strong>Title</strong> : ".$value."</br>";
+					//Les 4 lignes du dessous permettent de créer le lien pour accéder à la page internet publique du site TMDB.
 					$value = strtolower($value);
 					if(isset($_GET['movie'])) $email = "https://www.themoviedb.org/movie/".$_GET['movie']."-"."$value";
 					else $email = "https://www.themoviedb.org/movie/"."550"."-"."$value";
