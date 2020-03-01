98ff027d2fc899ef71a5b8b36b2bd41e8999fbaf
AlexandreSoulard <58270807+AlexandreSoulard@users.noreply.github.com>
2020-03-01 20:26:29 +0100

    Update phpcurl.php

--- THDB/phpcurl.php
+++ THDB/phpcurl.php
@@ -1,11 +1,9 @@
 <?php
 require_once("tp3-helpers.php");
-			//<?php
 			if(isset($argv[2])){
 				$split = explode("=", $argv[2]);
 				$array[$split[0]] = $split[1];
 			}
-			//if(isset($argv[2])) $array = array('language' => $argv[2]);
 			else $array = array('language' => 'en');
 			$content = json_decode(tmdbget("movie/550", $array), true);
 			if(isset($argv[1])) $content = json_decode(tmdbget($argv[1], $array), true);
